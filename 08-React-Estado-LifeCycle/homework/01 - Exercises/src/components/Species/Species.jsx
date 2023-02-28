d9661a993ad31fca55ec5a5d3c98ff0aaa7baf65
HX-ASandoval2 <auri.sandoval@gmail.com>
2023-02-28 19:02:36 -0300

    applyng HW8-Exercises styles

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
+// import styledSpecies from "./Species.module.css";
 
-export default function Species () {
-  return (
-    <div>
-
-    </div>
-  )
+export default function Species({ species, handleSpecies, handleAllSpecies }) {
+  // console.log(species);
+  return <div></div>
 }
