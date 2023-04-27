fd17a17bd85e89ebc44a868911a136a52e32653e
HX-ASandoval2 <auri.sandoval@gmail.com>
2023-04-27 20:59:10 -0300

    homework ES6 (pesadilla)

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,6 +9,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if(matchFunc(startEl))resultSet.push(startEl);
+   
+  //recorrer los hijos
+  for (let i = 0; i < startEl.children.length; i++) {
+    let resultado = traverseDomAndCollectElements(matchFunc,startEl.children[i])
+    
+    resultSet = [...resultSet, ...resultado]
+  }
+  return resultSet;
   
 };
 
@@ -16,26 +25,56 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
 
-var selectorTypeMatcher = function(selector) {
+var selectorTypeMatcher = function(selector) { //'#myId', '.myClass', 'img.imagen', 'img'
   // tu código aquí
-  
+  if(selector[0] === '#') return 'id';
+  else if(selector[0] === '.') return 'class'
+  // for (let i = 0; i < selector.length; i++) {
+  //   if(selector[i] === '.') return 'tag.class'
+  // }
+  // if (selector.indexOf(".") !== -1) return "tag.class";
+  // if (selector.includes(".")) return "tag.class";
+  if (selector.split( '.' ). length === 2 ) return 'tag.class'
+  return 'tag'
 };
+
 
 // NOTA SOBRE LA FUNCIÓN MATCH
 // recuerda, la función matchFunction devuelta toma un elemento como un
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function(selector) {
+var matchFunctionMaker = function(selector) { //'#myId', '.myClass', 'div'
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
+    // matchFunction = function(elem){
+    //   return '#' + elem.id === selector
+    // }
+    matchFunction = elem => `#${elem.id}` === selector
    
-  } else if (selectorType === "class") {
+  } else if (selectorType === "class") {//'.myClass'
+    // matchFunction = function(elem){//div
+    //   for (let i = 0; i < elem.classList.length; i++) {
+    //     if('.' + elem.classList[i] === selector) return true
+    //   }
+    //}
+    matchFunction = elem => elem.classList.contains(selector.substring(1))
+     
     
-  } else if (selectorType === "tag.class") {
+  } else if (selectorType === "tag.class") { //'div.myClass'
+    //destructuración
+    //separarlos por el puntico
+    //recursión
+    matchFunction = function(elem){ // ['div', 'myClass']
+      let [t,c] = selector.split('.') // [tag, class] -> t = tag, c = class
+      return matchFunctionMaker(t)(elem) && matchFunctionMaker('.' + c)(elem)
+    }
     
-  } else if (selectorType === "tag") {
+  } else if (selectorType === "tag") { //'div'
+    matchFunction = function(elem){
+      return elem.tagName.toLowerCase() === selector.toLowerCase() //--> 'div' === 'div
+    }
     
   }
   return matchFunction;
@@ -47,3 +86,7 @@ var $ = function(selector) {
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+
+
+
+//$('.myClass') -> document.querySelectorAll('.myClass')
